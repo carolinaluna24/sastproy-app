81e833ec270cef22d840f3cd9aaf6b07383dee84
carolinaluna24 <carolinaluna.tec@gmail.com>
2026-03-11 23:00:25 -0500

    Depuración de librerías

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
@@ -12,18 +11,10 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
-
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_AUTOLOGIN: string
-  readonly VITE_DEV_EMAIL: string
-  readonly VITE_DEV_PASSWORD: string
-}
